1575188100c74787b5ddf434e41419c2f4f60762
Vicki Hamm <vicki.cello@gmail.com>
2018-07-11 19:11:28 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,7 +28,7 @@ class Scraper
         :github => page.find { |i| i.include?("github") },
         :blog => page.find { |i| !i.include?("twitter") && !i.include?("github") && !i.include?("linkedin") },
         :profile_quote => Nokogiri::HTML(open(profile_url)).css("div.profile_quote").text
-        :bio => Nokogiri::HTML(open(profile_url)).css(".description-holder").css("p").text
+        :bio => Nokogiri::HTML(open(profile_url)).css("p").text
       }
 
   end
