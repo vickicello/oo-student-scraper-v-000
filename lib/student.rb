695e4fc9d9add3ee5d6c5c0a64849015bdc9e9ed
Vicki Hamm <vicki.cello@gmail.com>
2018-07-11 18:14:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,14 +7,28 @@ class Student
   # Student class shouldn't directly interact with the Scraper class––
   # it shouldn't call on the Scraper class in any of its methods or take
   # in the Scraper class itself as an argument.
-  
+
 
   def initialize(student_hash)
+    # use metaprogramming to assign the newly created student
+    # attributes and values in accordance with the key/value pairs of the hash.
+    #  Use the #send method to acheive this.
+    # This method should also add the newly created student to the
+    # Student class' @@all array of all students.
+    # You'll need to create this class variable and set it equal
+    # to an empty array at the top of your class.
+    # Push self into the array at the end of the #initialize method.
+
 
   end
 
   def self.create_from_collection(students_array)
-
+    # method should take in an array of hashes.
+    # we will call Student.create_from_collection with the
+    # return value of the Scraper.scrape_index_page method
+    # as the argument. The #create_from_collection method
+    # should iterate over the array of hashes and
+    # create a new individual student using each hash
   end
 
   def add_student_attributes(attributes_hash)
